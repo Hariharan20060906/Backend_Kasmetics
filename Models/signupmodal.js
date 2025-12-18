b79e09565d565123b72ff8870004ebc5e19a638f
Hareharan N <hareharan.n2024aids@sece.ac.in>
2025-12-18 10:08:26 +0530

    Delete Models directory

--- Models/signupmodal.js
+++
@@ -1,8 +0,0 @@
-const mangoose = require('mongoose');
-const userSchema = new mangoose.Schema({
-    firstname: String,
-    email: String,
-    phone: String,
-    password: String
-})
-module.exports = mangoose.model("User", userSchema)
